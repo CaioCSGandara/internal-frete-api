381c2d3c9728b9678b7364e1a3c895f5dd4393a0
CaioCSGandara <caiocsgandara@gmail.com>
2025-05-30 20:36:34 -0300

    iniciando projeto spring

---
+++ src/main/java/com/internal/frete/api/InternalFreteApiApplication.java
@@ -0,0 +1,13 @@
+package com.internal.frete.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalFreteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalFreteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internal/frete/api/InternalFreteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internal.frete.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalFreteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
