a9da34b02cc9bfc495995007181e346870e3971b
filipe mota <filipe.dmtm@puccampinas.edu.br>
2025-06-03 01:13:37 -0300

    inserindo arquivos de frete no pacote strategy

--- src/test/java/com/internal/frete/api/service/strategy/FreteNormalTest.java
+++ src/test/java/com/internal/frete/api/service/strategy/FreteNormalTest.java
@@ -1,4 +1,82 @@
 package com.internal.frete.api.service.strategy;
 
+import com.internal.frete.api.config.FreteValoresDefault;
+import com.internal.frete.api.dto.CalculoFreteRequest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class FreteNormalTest {
+
+    @Test
+    public void calcularFreteComEntradaValida() {
+        CalculoFreteStrategy freteNormal = new FreteNormal();
+
+        CalculoFreteRequest calculoFreteRequest = new CalculoFreteRequest();
+        calculoFreteRequest.setTipoFrete("normal");
+        calculoFreteRequest.setPeso(10);
+        calculoFreteRequest.setDistancia(100);
+
+        double resultado = freteNormal.calcular(calculoFreteRequest, FreteValoresDefault.getInstancia());
+
+        assertEquals(74.39, resultado, 0.01);
+    }
+
+    @Test
+    public void verificaFreteAplicavelComValoresValidos() {
+
+        CalculoFreteStrategy freteNormal = new FreteNormal();
+
+        CalculoFreteRequest calculoFreteRequest = new CalculoFreteRequest();
+        calculoFreteRequest.setTipoFrete("normal");
+        calculoFreteRequest.setPeso(10);
+        calculoFreteRequest.setDistancia(100);
+
+        boolean isFreteAplicavel = freteNormal.isFreteAplicavel(calculoFreteRequest, FreteValoresDefault.getInstancia());
+
+        assertTrue(isFreteAplicavel);
+    }
+
+    @Test
+    public void verificaTesteAplicavelComPesoAcimaDoLimite() {
+        CalculoFreteStrategy freteNormal = new FreteNormal();
+
+        CalculoFreteRequest calculoFreteRequest = new CalculoFreteRequest();
+        calculoFreteRequest.setTipoFrete("normal");
+        calculoFreteRequest.setPeso(101);
+        calculoFreteRequest.setDistancia(100);
+
+        boolean isFreteAplicavel = freteNormal.isFreteAplicavel(calculoFreteRequest, FreteValoresDefault.getInstancia());
+
+        assertFalse(isFreteAplicavel);
+    }
+
+    @Test
+    public void verificaTesteAplicavelComDistanciaAcimaDoLimite() {
+        CalculoFreteStrategy freteNormal = new FreteNormal();
+
+        CalculoFreteRequest calculoFreteRequest = new CalculoFreteRequest();
+        calculoFreteRequest.setTipoFrete("normal");
+        calculoFreteRequest.setPeso(10);
+        calculoFreteRequest.setDistancia(1501);
+
+        boolean isFreteAplicavel = freteNormal.isFreteAplicavel(calculoFreteRequest, FreteValoresDefault.getInstancia());
+
+        assertFalse(isFreteAplicavel);
+    }
+
+    @Test
+    public void verificaTesteAplicavelComDistanciaMenorQueAMinima() {
+        CalculoFreteStrategy freteNormal = new FreteNormal();
+
+        CalculoFreteRequest calculoFreteRequest = new CalculoFreteRequest();
+        calculoFreteRequest.setTipoFrete("normal");
+        calculoFreteRequest.setPeso(10);
+        calculoFreteRequest.setDistancia(9);
+
+        boolean isFreteAplicavel = freteNormal.isFreteAplicavel(calculoFreteRequest, FreteValoresDefault.getInstancia());
+
+        assertFalse(isFreteAplicavel);
+    }
 }
