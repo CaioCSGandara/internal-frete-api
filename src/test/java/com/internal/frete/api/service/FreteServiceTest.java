f23424790d25aaa0d045ad983934a90b28cd131e
filipe mota <filipe.dmtm@puccampinas.edu.br>
2025-06-05 09:19:39 -0300

    Arrumando sintaxe de notação no arquivo de Teste + Exclusão de pasta denecessária -- Filipe Mota

--- src/test/java/com/internal/frete/api/service/FreteServiceTest.java
+++ src/test/java/com/internal/frete/api/service/FreteServiceTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTests
+@SpringBootTest
 public class FreteServiceTest {
 
     @Autowired
